5431612aed1e207a52e30b2960b4b75ae9270d1e
Karina Sanchez <kasmcreativestudio@gmail.com>
2017-09-25 21:22:10 -0400

    Initial Commit

---
+++ devslopes course 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes course 2
+//
+//  Created by Karina Sanchez on 9/25/17.
+//  Copyright © 2017 Kasm Creative Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
